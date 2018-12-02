f6ca46d915951d9031704311ed8398bb6d54333d
tadeujr <tadeu_junior@outlook.com>
2018-12-02 17:13:37 -0200

    Menu

--- source/ControleMedicamentos/src/posto/control/Menu.java
+++ source/ControleMedicamentos/src/posto/control/Menu.java
@@ -25,92 +25,97 @@ public class Menu {
         FuncionarioSql controleFuncionario = new FuncionarioSql();
 
         // String login = JOptionPane.showInputDialog("Login");
-        String menu = JOptionPane.showInputDialog("Senha");
+        String menu = JOptionPane.showInputDialog("\t\t           Menu Principal"+ "\nCadastra Funcionario: 1 "
+                        + "\nOpções Medicamento: 2"
+                        + "\nDeletar Medicamento: 3" + "\nSair: 0");
         int opcao = Integer.parseInt(menu);
         //verificar login
-        // while (opcao != 0) {
+        while (opcao != 0) {
 
-        //Opções Menu
-        if (opcao == 1) {//Cadastrar Funcionario            
-            String nome = JOptionPane.showInputDialog("Nome");
-            String cpf = JOptionPane.showInputDialog("CPF");
-            String senhaF = JOptionPane.showInputDialog("Senha");
-            String loginF = JOptionPane.showInputDialog("Login");
-            String tipo = JOptionPane.showInputDialog("Tipo:(Gerente ou Atendente");
-            String email = JOptionPane.showInputDialog("Email");
-            String telefone = JOptionPane.showInputDialog("Telefone");
-            String endereco = JOptionPane.showInputDialog("Endereço");
+            //Opções Menu
+            if (opcao == 1) {//Cadastrar Funcionario            
+                String nome = JOptionPane.showInputDialog("Nome");
+                String cpf = JOptionPane.showInputDialog("CPF");
+                String senhaF = JOptionPane.showInputDialog("Senha");
+                String loginF = JOptionPane.showInputDialog("Login");
+                String tipo = JOptionPane.showInputDialog("Tipo:(Gerente ou Atendente");
+                String email = JOptionPane.showInputDialog("Email");
+                String telefone = JOptionPane.showInputDialog("Telefone");
+                String endereco = JOptionPane.showInputDialog("Endereço");
 
-            Funcionario nFuncionario = new Funcionario(loginF, senhaF, tipo, nome, email, telefone, endereco, cpf); // pesquisa metodo de crptografia
-            controleFuncionario.cadastrarFuncionario(nFuncionario);
+                Funcionario nFuncionario = new Funcionario(loginF, senhaF, tipo, nome, email, telefone, endereco, cpf); // pesquisa metodo de crptografia
+                controleFuncionario.cadastrarFuncionario(nFuncionario);
 
-        }
-
-        if (opcao == 2) {//Cadastrar Medicamento FUNCIONANDO
-
-            String opCrud = JOptionPane.showInputDialog("Cadastra Medicamento: 1 "
-                    + "\nExibir Medicamento: 2");
-
-            int opcCrud = Integer.parseInt(opCrud);
-
-            if (opcCrud == 1) {
-                String nomeMedicamento = JOptionPane.showInputDialog("Medicamento");
-                String validade = JOptionPane.showInputDialog("validade");
-                String lote = JOptionPane.showInputDialog("lote");
-                String qtd = JOptionPane.showInputDialog("Quantidade");
-                String descricao = JOptionPane.showInputDialog("Descricao");
-                int qtdDisponivel = Integer.parseInt(qtd);
-
-                /*CRIAÇÃO MEDICAMENTO -> SALVAnDO EM ARQUIVO E ABRINDO*/
-                Medicamento nMedicamento = new Medicamento(nomeMedicamento, descricao, validade, lote, qtdDisponivel);
-
-                /*enviando objeto para cadastrar*/
-                controleMedicamento.cadastrarMedicamento(nMedicamento);
             }
 
-            if (opcCrud == 2) {//MOSTRA MEDICAMENTO
-                controleMedicamento.exibirMedicamentos();
+            if (opcao == 2) {//Cadastrar Medicamento FUNCIONANDO
+
+                String opCrud = JOptionPane.showInputDialog("Cadastra Medicamento: 1 "
+                        + "\nExibir Medicamento: 2"
+                        + "\nCadastrar Cliente: 3" + "\nSair: 4");
+
+                int opcCrud = Integer.parseInt(opCrud);
+
+                if (opcCrud == 1) {
+                    String nomeMedicamento = JOptionPane.showInputDialog("Medicamento");
+                    String validade = JOptionPane.showInputDialog("validade");
+                    String lote = JOptionPane.showInputDialog("lote");
+                    String qtd = JOptionPane.showInputDialog("Quantidade");
+                    String descricao = JOptionPane.showInputDialog("Descricao");
+                    int qtdDisponivel = Integer.parseInt(qtd);
+
+                    /*CRIAÇÃO MEDICAMENTO -> SALVAnDO EM ARQUIVO E ABRINDO*/
+                    Medicamento nMedicamento = new Medicamento(nomeMedicamento, descricao, validade, lote, qtdDisponivel);
+
+                    /*enviando objeto para cadastrar*/
+                    controleMedicamento.cadastrarMedicamento(nMedicamento);
+                }
+
+                if (opcCrud == 2) {//MOSTRA MEDICAMENTO
+                    controleMedicamento.exibirMedicamentos();
+                }
+
+                if (opcCrud == 3) {//Deletar Medicamento NOME FUNCIONANDO
+                    String nomeMedicamento = JOptionPane.showInputDialog("Nome Medicamento");
+                    controleMedicamento.deleteMedicamento(nomeMedicamento);
+                }
+
+                if (opcCrud == 4) {//voltar
+                    menu = JOptionPane.showInputDialog("Senha");
+                    opcao = Integer.parseInt(menu);
+                }
             }
 
-            if (opcCrud == 3) {
-                String nomeMedicamento = JOptionPane.showInputDialog("Medicamento");
-                controleMedicamento.deleteMedicamento(nomeMedicamento);
+            if (opcao == 3) {//Cria Cliente FUNCIONANDO
+
+                String nome = JOptionPane.showInputDialog("Nome");
+                String cpf = JOptionPane.showInputDialog("CPF");
+                String senhaF = JOptionPane.showInputDialog("Senha");
+                String loginF = JOptionPane.showInputDialog("Login");
+                String tipo = JOptionPane.showInputDialog("Tipo:(Gerente ou Atendente");
+                String email = JOptionPane.showInputDialog("Email");
+                String telefone = JOptionPane.showInputDialog("Telefone");
+                String endereco = JOptionPane.showInputDialog("Endereço");
+                String cartaoSus = JOptionPane.showInputDialog("Cartão SUS");
+                Cliente cliente = new Cliente(nome, email, telefone, endereco, cpf, cartaoSus);
+                controleCliente.cadastrarCliente(cliente);
+
             }
 
+            if (opcao == 4) {// LISTA FUNCIONADO
+
+            }
+
+            if (opcao == 5) {//CRUD MEDICAMENTO
+
+            }
+
+                menu = JOptionPane.showInputDialog("Cadastra Medicamento: 1 "
+                        + "\nExibir Medicamento: 2"
+                        + "\nCadastrar Cliente: 3" + "\nSair: 4");
+
+                opcao = Integer.parseInt(menu);
         }
 
-        if (opcao == 3) {//Cria Cliente FUNCIONANDO
-
-            String nome = JOptionPane.showInputDialog("Nome");
-            String cpf = JOptionPane.showInputDialog("CPF");
-            String senhaF = JOptionPane.showInputDialog("Senha");
-            String loginF = JOptionPane.showInputDialog("Login");
-            String tipo = JOptionPane.showInputDialog("Tipo:(Gerente ou Atendente");
-            String email = JOptionPane.showInputDialog("Email");
-            String telefone = JOptionPane.showInputDialog("Telefone");
-            String endereco = JOptionPane.showInputDialog("Endereço");
-            String cartaoSus = JOptionPane.showInputDialog("Cartão SUS");
-            Cliente cliente = new Cliente(nome, email, telefone, endereco, cpf, cartaoSus);
-            controleCliente.cadastrarCliente(cliente);
-
-        }
-
-        if (opcao == 4) {// LISTA FUNCIONADO
-
-        }
-
-        if (opcao == 5) {//CRUD MEDICAMENTO
-
-        }
-
-        /* menu = JOptionPane.showInputDialog("Cadastra Fucionário: 1 "
-                + "\nCadastrar Medicamento: 2"
-                + "\nMostrar Medicamentos: 3"
-                + "\nMostrar Funcionário: 4"
-                + "\nSair: 0");
-
-        opcao = Integer.parseInt(menu);*/
     }
-
-    //}
 }
